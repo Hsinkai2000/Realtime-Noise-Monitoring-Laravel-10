1ad2b4e67fc3fc192681fc03e24136362870d700
Hsin Kai <hsinkai2000@gmail.com>
2024-11-20 15:33:58 +0800

    feat: add English locale configuration for AirDatepicker in measurement point view

--- public/js/measurement_point.js
+++ public/js/measurement_point.js
@@ -3,6 +3,52 @@ if (window.location.port) {
     baseUri += `:${window.location.port}`;
 }
 var dpMin, dpMax;
+const localeEn = {
+    days: [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+    ],
+    daysShort: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+    daysMin: ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"],
+    months: [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ],
+    monthsShort: [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec",
+    ],
+    today: "Today",
+    clear: "Clear",
+    dateFormat: "MM/dd/yyyy",
+    timeFormat: "hh:mm aa",
+    firstDay: 0,
+};
 
 function set_tables(data) {
     var noise_meter_table = new Tabulator("#noise_meter_table", {
