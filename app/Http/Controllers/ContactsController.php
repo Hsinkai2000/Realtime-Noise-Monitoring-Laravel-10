c2e225bb17e454ac65463fa43a7c2f4cf86c4391
Hsin Kai <hsinkai2000@gmail.com>
2024-12-30 16:36:31 +0800

    Add integer validation rule for phone_number in contact validation

--- app/Http/Controllers/ContactsController.php
+++ app/Http/Controllers/ContactsController.php
@@ -88,7 +88,7 @@ class ContactsController extends Controller
         return $request->validate([
             'contact_person_name' => 'required',
             'designation' => 'required',
-            'phone_number' => 'digits:8'
+            'phone_number' => 'digits:8|integer'
         ]);
     }
 }
