690f1d32671c3c4c2cb174ff65161fc0688a30ae
Hsin Kai <hsinkai2000@gmail.com>
2025-02-28 19:52:09 +0800

    Integrate Chart.js for dynamic graph rendering in noise data report

--- resources/views/pdfs/noise-data-report.blade.php
+++ resources/views/pdfs/noise-data-report.blade.php
@@ -177,9 +177,23 @@
 
             <div class="reportGraph"><canvas id="canvas{{ $date->format('d-m-Y') }}"></canvas></div>
             <script>
-                window.onload = function() {
-                    drawGraphs("canvas{{ $date->format('d-m-Y') }}");
-                };
+                new Chart(
+                    document.getElementById("canvas{{ $date->format('d-m-Y') }}"), {
+                        "responsive": false,
+                        "type": "line",
+                        "data": {
+                            "labels": ["January", "February", "March", "April", "May", "June", "July"],
+                            "datasets": [{
+                                "label": "My First Dataset",
+                                "data": [65, 59, 80, 81, 56, 55, 40],
+                                "fill": false,
+                                "borderColor": "rgb(75, 192, 192)",
+                                "lineTension": 0.1
+                            }]
+                        },
+                        "options": {}
+                    }
+                );
             </script>
             <br>
             {{-- <x-pdfs.partials-report-chart :measurementPoint="$measurementPoint" :date="$date->copy()" /> --}}
