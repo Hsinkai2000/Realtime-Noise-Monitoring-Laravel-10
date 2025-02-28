66f97a5f1a34027bea2071fd118aab617e357730
Hsin Kai <hsinkai2000@gmail.com>
2025-02-28 19:54:29 +0800

    Implement Chart.js rendering in partials for noise data report

--- resources/views/pdfs/noise-data-report.blade.php
+++ resources/views/pdfs/noise-data-report.blade.php
@@ -175,28 +175,8 @@
                 <x-pdfs.partials-report-data :measurementPoint="$measurementPoint" :date="$date" />
             </div>
 
-            <div class="reportGraph"><canvas id="canvas{{ $date->format('d-m-Y') }}"></canvas></div>
-            <script>
-                new Chart(
-                    document.getElementById("canvas{{ $date->format('d-m-Y') }}"), {
-                        "responsive": false,
-                        "type": "line",
-                        "data": {
-                            "labels": ["January", "February", "March", "April", "May", "June", "July"],
-                            "datasets": [{
-                                "label": "My First Dataset",
-                                "data": [65, 59, 80, 81, 56, 55, 40],
-                                "fill": false,
-                                "borderColor": "rgb(75, 192, 192)",
-                                "lineTension": 0.1
-                            }]
-                        },
-                        "options": {}
-                    }
-                );
-            </script>
             <br>
-            {{-- <x-pdfs.partials-report-chart :measurementPoint="$measurementPoint" :date="$date->copy()" /> --}}
+            <x-pdfs.partials-report-chart :measurementPoint="$measurementPoint" :date="$date->copy()" />
             {{-- <img src="{{ route('chart-image', ['date' => $date, 'measurementPointID' => $measurementPoint->id]) }}"
                 alt="Chart"> --}}
 
