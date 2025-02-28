9c4eee64549e6b8bdc61287f7388bdae3f13ece1
Hsin Kai <hsinkai2000@gmail.com>
2025-02-28 20:38:13 +0800

    Refactor report chart script to improve readability and maintainability

--- resources/views/components/pdfs/partials-report-chart.blade.php
+++ resources/views/components/pdfs/partials-report-chart.blade.php
@@ -1,73 +1,5 @@
 <div class="reportGraph"><canvas id="canvas{{ $date->format('d-m-Y') }}"></canvas></div>
 
-<script>
-    new Chart(
-        document.getElementById("canvas{{ $date->format('d-m-Y') }}"), {
-            "responsive": false,
-            "type": "line",
-            "data": {
-                "datasets": [{
-                    "label": "Limit",
-                    "data": generateLimitData(),
-                    "borderColor": "rgba(255, 0, 0, 1)",
-                    "pointRadius": 0,
-                    "borderWidth": 2,
-                    "fill": false,
-                    "steppedLine": true
-                }, {
-                    "label": "LAeq 5min",
-                    "data": [{
-                            x: '{{ $date->format('Y-m-d') }}T07:00:00',
-                            y: 60
-                        },
-                        {
-                            x: '{{ $date->format('Y-m-d') }}T08:00:00',
-                            y: 70
-                        },
-                        {
-                            x: '{{ $date->format('Y-m-d') }}T09:00:00',
-                            y: 65
-                        }
-                    ],
-
-                    "borderColor": "rgba(0, 0, 255, 1)",
-                    "borderWidth": 2,
-                    "pointRadius": 0,
-                    "spanGaps": true,
-                    "fill": false
-                }]
-            },
-            "options": {
-                "scales": {
-                    "xAxes": [{
-                        "type": "time",
-                        "time": {
-                            "unit": "hour",
-                            "stepSize": 1,
-                            "min": "{{ $date->format('Y-m-d') }}T07:00:00",
-                            "max": "{{ $date->copy()->addDay()->format('Y-m-d') }}T06:55:00",
-                            "displayFormats": {
-                                "hour": "HH:mm"
-                            }
-                        },
-                        "ticks": {
-                            "autoSkip": true,
-                            "maxTicksLimit": 24
-                        }
-                    }],
-                    "yAxes": [{
-                        "ticks": {
-                            "beginAtZero": true,
-                            "max": 120
-                        }
-                    }]
-                },
-                "legend": {
-                    "display": true
-                }
-            }
-        });
-</script>
 
 <script>
     function generateLimitData() {
@@ -143,56 +75,70 @@
     }
 
 
-    // var ctx = document.getElementById('myChart{{ $date->format('d-m-Y') }}').getContext('2d');
-    // var myChart = new Chart(ctx, {
-    //     responsive: false,
-    //     type: 'line',
-    //     data: {
-    //         datasets: [{
-    //             label: 'Limit',
-    //             data: generateLimitData(),
-    //             borderColor: 'rgba(255, 0, 0, 1)',
-    //             pointRadius: 0,
-    //             borderWidth: 2,
-    //             fill: false,
-    //             steppedLine: true
-    //         }, {
-    //             label: 'LAeq 5min',
-    //             data: generateNoiseData(),
-    //             borderColor: 'rgba(0, 0, 255, 1)',
-    //             borderWidth: 2,
-    //             pointRadius: 0,
-    //             spanGaps: true,
-    //             fill: false
-    //         }]
-    //     },
-    //     options: {
-    //         scales: {
-    //             xAxes: [{
-    //                 type: 'time',
-    //                 time: {
-    //                     unit: 'minute',
-    //                     stepSize: 5,
-    //                     displayFormats: {
-    //                         minute: 'HH:mm'
-    //                     }
-    //                 },
-    //                 ticks: {
-    //                     autoSkip: true,
-    //                     maxTicksLimit: 8
-    //                 }
-    //             }],
-    //             yAxes: [{
-    //                 ticks: {
-    //                     beginAtZero: true,
-    //                     max: 120
-    //                 }
-    //             }]
-    //         },
-    //         legend: {
-    //             display: true
-    //         }
-    //     }
+    new Chart(
+        document.getElementById("canvas{{ $date->format('d-m-Y') }}"), {
+            "responsive": false,
+            "type": "line",
+            "data": {
+                "datasets": [{
+                    "label": "Limit",
+                    "data": generateLimitData(),
+                    "borderColor": "rgba(255, 0, 0, 1)",
+                    "pointRadius": 0,
+                    "borderWidth": 2,
+                    "fill": false,
+                    "steppedLine": true
+                }, {
+                    "label": "LAeq 5min",
+                    "data": [{
+                            x: '{{ $date->format('Y-m-d') }}T07:00:00',
+                            y: 60
+                        },
+                        {
+                            x: '{{ $date->format('Y-m-d') }}T08:00:00',
+                            y: 70
+                        },
+                        {
+                            x: '{{ $date->format('Y-m-d') }}T09:00:00',
+                            y: 65
+                        }
+                    ],
 
-    // });
+                    "borderColor": "rgba(0, 0, 255, 1)",
+                    "borderWidth": 2,
+                    "pointRadius": 0,
+                    "spanGaps": true,
+                    "fill": false
+                }]
+            },
+            "options": {
+                "scales": {
+                    "xAxes": [{
+                        "type": "time",
+                        "time": {
+                            "unit": "hour",
+                            "stepSize": 1,
+                            "min": "{{ $date->format('Y-m-d') }}T07:00:00",
+                            "max": "{{ $date->copy()->addDay()->format('Y-m-d') }}T06:55:00",
+                            "displayFormats": {
+                                "hour": "HH:mm"
+                            }
+                        },
+                        "ticks": {
+                            "autoSkip": true,
+                            "maxTicksLimit": 24
+                        }
+                    }],
+                    "yAxes": [{
+                        "ticks": {
+                            "beginAtZero": true,
+                            "max": 120
+                        }
+                    }]
+                },
+                "legend": {
+                    "display": true
+                }
+            }
+        });
 </script>
